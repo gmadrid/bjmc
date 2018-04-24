e9ede12aec3dec757bd07936dc616adde201dbb4
George Madrid <gmadrid@gmail.com>
2018-04-24 10:23:55 -0400

    Adding game.h, but nothing works.

---
+++ src/lib/game.h
@@ -0,0 +1,12 @@
+#ifndef GAME_H
+#define GAME_H
+
+class Game {
+  // 1. Make a Dealer player with dealer strategy.
+  // 2. Make a Player with basic strategy.
+  // 3. Run the Player.
+  // 4. Run the Dealer.
+  // 5. Find winner.
+}
+
+#endif  // GAME_H
